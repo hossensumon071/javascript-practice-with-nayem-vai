682beaa5b0c5a202ceea8d3779380042a2aa3fb7
hossensumon071 <hossensumon071@gmail.com>
2021-03-03 12:21:51 +0600

    oparetor in js

---
+++ 1.05oparator.js
@@ -0,0 +1,78 @@
+// Arithmetic Operator
+
+// +,-,*,/, %, ++, --
+
+// var a = 10
+// var b = 3
+
+// console.log(a % b)
+
+// Incremental 
+// Pre Incremental - Post Incremental
+
+// console.log(++a)
+// console.log(a++)
+
+
+// Assignment oparetor
+
+// var a = 10
+// var b = 20
+
+// // a = a + b
+// a += b
+
+// console.log(a)
+
+// a -= b
+
+// console.log(a)
+
+// a /= b
+
+// console.log(a)
+
+// a %= b
+
+// console.log(a)
+
+
+// comparision Operators
+
+
+var a = 10
+var b = 20
+
+console.log(a == b)
+console.log(a != b)
+
+console.log(a > b)
+console.log(a < b)
+
+console.log(a >= b)
+
+var c = '50'
+var d = 50
+
+console.log(c == d)
+console.log(c === d)
+console.log(c !== d)
+
+
+// Logical Operators
+
+// &&
+// ||
+// !
+
+// Bitwise Operator
+
+// &
+// |
+// ~
+// ^
+// <<
+
+// type of oparetor
+
+console.log(typeof 'String')
