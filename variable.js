b5f28b361952ca7bd84431f35de65a5604c38d49
hossensumon071 <hossensumon071@gmail.com>
2021-03-03 10:10:35 +0600

    add reseved keywods

--- variable.js
+++ variable.js
@@ -8,3 +8,8 @@
 // console.log('His age is only ' + age)
 // console.log(name+' is creating Javascript Course For All')
 // console.log('But his age is only '+ age)
+
+
+var mathNumber = 10
+var accountNumberDetailsId = 104
+var account_number_details_id
