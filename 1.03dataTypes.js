cc57f1b407d78e7c2886eb532a19d37588608a41
hossensumon071 <hossensumon071@gmail.com>
2021-03-03 10:39:13 +0600

    knowing about string

--- 1.03dataTypes.js
+++ 1.03dataTypes.js
@@ -23,13 +23,29 @@
 // Numbers
 
 
-var n = 1451
-var f = 3.14
-var nn = Number('45.45')
+// var n = 1451
+// var f = 3.14
+// var nn = Number('45.45')
 
-console.log(nn)
-console.log(Number.parseInt(nn))
-console.log(Number.MAX_VALUE)
-console.log(Number.MIN_SAFE_INTEGER)
-console.log(1 / 0)
-console.log('abc' * 10)
+// console.log(nn)
+// console.log(Number.parseInt(nn))
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(1 / 0)
+// console.log('abc' * 10)
+
+
+// ***********   String   ***********
+
+
+var str = 'String'
+var str2 = "String"
+var str3 = `string`
+
+
+var str4 = String('sddshbkjg')
+var str5 = String(156)
+var str6 = String(3.1416)
+
+
+console.log(str4, str5, str6)
