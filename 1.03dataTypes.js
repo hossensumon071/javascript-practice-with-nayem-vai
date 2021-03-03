e4dba0c9c7926127466d3aea43945115a90d26ca
hossensumon071 <hossensumon071@gmail.com>
2021-03-03 10:31:56 +0600

    practice with number

---
+++ 1.03dataTypes.js
@@ -0,0 +1,35 @@
+// // primitive datatypes
+
+// Number
+// // 10, 3,5,6,3
+// String
+// // 'this is a text' + "This is also Text"
+// Boolean
+// // true false
+
+// undefined
+// // null
+
+
+
+
+// // object types of data types
+
+// Array 
+// Object
+// Function
+
+
+// Numbers
+
+
+var n = 1451
+var f = 3.14
+var nn = Number('45.45')
+
+console.log(nn)
+console.log(Number.parseInt(nn))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(1 / 0)
+console.log('abc' * 10)
