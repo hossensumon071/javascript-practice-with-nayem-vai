0cf2c6e0761a2efe0a616782beebe00658ca31be
hossensumon071 <hossensumon071@gmail.com>
2021-03-08 10:58:25 +0600

    what is comments practice with nayem vai

---
+++ 1.09comments.js
@@ -0,0 +1,14 @@
+// var name = 'sumon hossen'
+// var age = 23
+
+// // console.log(name)
+// // console.log(age)
+
+// console.log(name +' knows javascript.')
+// console.log('His age is only ' + age)
+// console.log(name+' is creating Javascript Course For All')
+// console.log('But his age is only '+ age)
+
+
+
+// practice with comments what is comments and everything i know from stack school 
