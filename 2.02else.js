a67ad6eca387de1b4e61540497b7f4cd7567d235
hossensumon071 <hossensumon071@gmail.com>
2021-03-08 12:07:31 +0600

    know about else condition else.js

---
+++ 2.02else.js
@@ -0,0 +1,29 @@
+var a = 10
+var b = 20
+
+// a > b true false 
+
+
+if (a > b) {
+    console.log('A is greater than B')
+} else {
+    console.log('B is greater than A')
+}
+
+
+if (a < b) {
+    console.log('B is greater than A')
+} else {
+    console.log('A is greater than B')
+}
+
+
+var n = 50 
+
+if (n % 2 == 0) {
+    console.log(n + ' is Even Number')
+}
+
+if (n % 2 == 1) {
+    console.log(n + ' is Odd Number')
+}
