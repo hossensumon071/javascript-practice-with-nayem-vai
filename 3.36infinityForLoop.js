e5fc0b2dc2e51b5a7405f3e493ca76f67e9691c4
hossensumon071 <hossensumon071@gmail.com>
2021-03-28 10:20:20 +0600

    infinity for loop

---
+++ 3.36infinityForLoop.js
@@ -0,0 +1,13 @@
+// Infinity For Loop 
+
+
+while (true) {
+    var rand  = Math.floor(Math.random() * 10 + 1)
+    if (rand === 9) {
+        console.log('Winner Winner Chicken Dinner')
+        break
+    } else {
+        console.log('You have got ' + rand)
+    }
+}
+
